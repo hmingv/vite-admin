e78a7130839d7fbe9534e790ef369b35ee0a5aeb
无聊的子木君 <vincentleecn@outlook.com>
2022-04-07 00:18:40 +0800

    build: 初始化项目

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const loginForm = ref({
+  username: "",
+  password: "",
+});
+
+const loginFormRef: any = ref({});
+</script>
+
+<template>
+  <div>
+    <n-form ref="loginFormRef" :modal="loginForm">
+      <n-form-item path="username" label="用户名">
+        <n-input></n-input>
+      </n-form-item>
+      <n-form-item path="password" label="密码">
+        <n-input></n-input>
+      </n-form-item>
+      <n-form-item>
+        <n-button>登录</n-button>
+      </n-form-item>
+    </n-form>
+  </div>
+</template>
